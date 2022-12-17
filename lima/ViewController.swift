28573dee3d885e360f6895427fb11d2f2a53354f
Sergey Ratiashvili <serg.s.r@gmail.com>
2022-12-17 22:18:55 +0100

    Initial Commit

---
+++ lima/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  lima
+//
+//  Created by Ratiashvili Sergey on 17.12.22..
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
